e2c03b0f3fdeec66efd58d0dc31d90620b5d60ce
SharmaKalpesh <108869652+SharmaKalpesh@users.noreply.github.com>
2024-03-12 14:45:34 +0530

    12/03/2024

--- app/src/main/java/com/privatepe/host/Zego/VideoChatZegoActivity.java
+++ app/src/main/java/com/privatepe/host/Zego/VideoChatZegoActivity.java
@@ -183,7 +183,7 @@ public class VideoChatZegoActivity extends BaseActivity implements ApiResponseIn
         hideStatusBar(getWindow(), true);
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
         networkCheck = new NetworkCheck();
         setContentView(R.layout.videochat_new);
 
@@ -220,7 +220,6 @@ public class VideoChatZegoActivity extends BaseActivity implements ApiResponseIn
         };
         try {
             Home.inviteClosedIs.observe(this, inviteObserver);
-
         } catch (Exception e) {
 
         }
